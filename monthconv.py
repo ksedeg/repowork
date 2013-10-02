9e924d44ee7c39ea4dabc47ab1a4e19ad1f3c587
Ksenia Degteva <ksedeg@cs.spbstu.ru>
2013-10-02 23:00:23 +0400

    Created monthconv.py

---
+++ monthconv.py
@@ -0,0 +1,11 @@
+def MonthConv():
+	dmonth={"January":1,"February":2,"March":3,"April":4,"May":5, "June":6,
+		"July":7, "August":8, "September":9, "October":10, 
+		"November":11, "December":12}
+	m=raw_input("Please, enter month: ")
+	m=m[0].upper()+m[1:].lower()
+	if dmonth.has_key(m)==True:
+		print("Month number: %s" %dmonth[m])
+	else:
+		print("Error!Please check the entered value.")
+MonthConv()
